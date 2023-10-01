3c2ca9dc5573beda10208ca6547ce565d505b4fe
Philipp Alber <info@philippalber.de>
2023-10-01 13:41:57 +0200

    Add feature

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -18,63 +18,137 @@ const setOutputMock = jest.spyOn(core, 'setOutput')
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
-
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
+  it('should handle empty arrays for entity names and values', async () => {
+    // Mock the inputs with empty arrays
+    getInputMock.mockImplementation((name: string) => {
       switch (name) {
-        case 'milliseconds':
-          return '500'
+        case 'file-path':
+          return 'file.json'
+        case 'entity-names':
+          return ''
+        case 'entity-values':
+          return ''
         default:
           return ''
       }
     })
 
+    // Call the action's main function
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
+    // Verify that core functions were called as expected
+    expect(getInputMock).toHaveBeenCalledWith('entity-names')
+    expect(getInputMock).toHaveBeenCalledWith('entity-values')
+    expect(setFailedMock).toHaveBeenCalledWith(
+      'The names and values must not be empty'
     )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
+    expect(debugMock).not.toHaveBeenCalled()
+  })
+
+  it('should handle errors when writing to the file', async () => {
+    // Mock the inputs
+    getInputMock.mockImplementation((name: string) => {
+      switch (name) {
+        case 'file-path':
+          return 'invalid/path/file.json'
+        case 'entity-names':
+          return 'name1,name2,name3'
+        case 'entity-values':
+          return 'value1,value2,value3'
+        default:
+          return ''
+      }
+    })
+
+    // Mock fs.writeFile to simulate an error
+    const writeFileMock = jest
+      .spyOn(require('node:fs'), 'writeFile')
+      .mockImplementation((path, data, callback: any) => {
+        callback(new Error('File write error'))
+      })
+
+    // Call the action's main function
+    await main.run()
+
+    // Verify that core functions were called as expected
+    expect(getInputMock).toHaveBeenCalledWith('file-path')
+    expect(getInputMock).toHaveBeenCalledWith('entity-names')
+    expect(getInputMock).toHaveBeenCalledWith('entity-values')
+    expect(setFailedMock).toHaveBeenCalledWith('File write error')
+    // Verify that fs.writeFile was called with the correct arguments
+    expect(writeFileMock).toHaveBeenCalledWith(
+      'invalid/path/file.json',
+      '{"name1":"value1","name2":"value2","name3":"value3"}',
+      expect.any(Function)
     )
   })
 
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
+  it('should handle an invalid number of names and values', async () => {
+    // Mock the inputs
+    getInputMock.mockImplementation((name: string) => {
       switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
+        case 'file-path':
+          return 'file.json'
+        case 'entity-names':
+          return 'name1,name2,name3'
+        case 'entity-values':
+          return 'value1,value2'
         default:
           return ''
       }
     })
 
+    // Call the action's main function
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
+    // Verify that core functions were called as expected
+    expect(getInputMock).toHaveBeenCalledWith('entity-names')
+    expect(getInputMock).toHaveBeenCalledWith('entity-values')
+    expect(setFailedMock).toHaveBeenCalledWith(
+      'The number of names and values must be the same'
+    )
+    expect(debugMock).not.toHaveBeenCalled()
+  })
+
+  it('should log when finished', async () => {
+    // Mock the inputs
+    getInputMock.mockImplementation((name: string) => {
+      switch (name) {
+        case 'file-path':
+          return 'file.json'
+        case 'entity-names':
+          return 'name1,name2,name3'
+        case 'entity-values':
+          return 'value1,value2,value3'
+        default:
+          return ''
+      }
+    })
+
+    // Mock fs.writeFile to simulate a successful write
+    const writeFileMock = jest
+      .spyOn(require('node:fs'), 'writeFile')
+      .mockImplementation((path, data, callback: any) => {
+        callback(null)
+      })
+
+    // Call the action's main function
+    await main.run()
+
+    // Verify that core functions were called as expected
+    expect(getInputMock).toHaveBeenCalledWith('entity-names')
+    expect(getInputMock).toHaveBeenCalledWith('entity-values')
+    expect(debugMock).toHaveBeenCalledWith('Created file at file.json')
+    // Verify that fs.writeFile was called with the correct arguments
+    expect(writeFileMock).toHaveBeenCalledWith(
+      'file.json',
+      '{"name1":"value1","name2":"value2","name3":"value3"}',
+      expect.any(Function)
     )
   })
 })
